62eb471f5573bfa85bc6dc9a76d2f7d387817063
Orlan Quijada <orlantquijada@gmail.com>
2020-11-26 14:39:50 +0800

    added manager

--- backend/users/managers.py
+++ backend/users/managers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import BaseUserManager
 
+from .choices import UserType
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **other_fields):
@@ -14,6 +16,42 @@ class UserManager(BaseUserManager):
 
         user.is_staff = True
         user.is_superuser = True
+
+        user.save()
+
+        return user
+
+    def create_end_user(self, email, password, **other_fields):
+        user = self.create_user(email, password, **other_fields)
+
+        user.user_type = UserType.END_USER
+
+        user.save()
+
+        return user
+
+    def create_department(self, email, password, **other_fields):
+        user = self.create_user(email, password, **other_fields)
+
+        user.user_type = UserType.DEPARTMENT
+
+        user.save()
+
+        return user
+
+    def create_imdc(self, email, password, **other_fields):
+        user = self.create_user(email, password, **other_fields)
+
+        user.user_type = UserType.IMDC
+
+        user.save()
+
+        return user
+
+    def create_president(self, email, password, **other_fields):
+        user = self.create_user(email, password, **other_fields)
+
+        user.user_type = UserType.PRESIDENT
 
         user.save()
 
