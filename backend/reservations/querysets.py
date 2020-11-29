b9358a6994b8e80c3cc106f3ae42e06a0fe7c732
Orlan Quijada <orlantquijada@gmail.com>
2020-11-30 05:59:28 +0800

    idk unsa nani

--- backend/reservations/querysets.py
+++ backend/reservations/querysets.py
@@ -1,19 +1,38 @@
 from django.db.models import Q, QuerySet
 
+from backend.users.choices import UserType
+
 
 class ReservationQuerySet(QuerySet):
-    def accepted(self):
+    def accepted(self, for_user: str = UserType.PRESIDENT):
+        if for_user in (UserType.DEPARTMENT, UserType.IMDC):
+            return self.filter(is_accepted_department=True)
+
         return self.filter(
             is_accepted_department=True,
             is_accepted_imdc=True,
             is_accepted_president=True,
         )
 
-    def pending(self):
+    def pending(self, for_user: str = UserType.DEPARTMENT):
+        if for_user == UserType.IMDC:
+            return self.filter(
+                is_accepted_department=True,
+                is_accepted_imdc=None,
+                is_accepted_president=None,
+            )
+
+        if for_user == UserType.PRESIDENT:
+            return self.filter(
+                is_accepted_department=True,
+                is_accepted_imdc=True,
+                is_accepted_president=None,
+            )
+
         return self.filter(
-            Q(is_accepted_department=None)
-            | Q(is_accepted_imdc=None)
-            | Q(is_accepted_president=None)
+            is_accepted_department=None,
+            is_accepted_imdc=None,
+            is_accepted_president=None,
         )
 
     def declined(self):
@@ -22,3 +41,6 @@ class ReservationQuerySet(QuerySet):
             | Q(is_accepted_imdc=False)
             | Q(is_accepted_president=False)
         )
+
+    def from_department(self, department_id: int):
+        return self.filter(room__department_id=department_id)
